3d3713d5317fb5a486e2e8db348f51a1cdfd3389
Thomas Kaegi <tk@meeloo.ch>
2016-07-15 12:17:04 +0200

    $detect umbenannt in $mobiledetect, isiOS + isAndroidOS entfernt, Methoden für Browser hinzugefügt

--- lib/class.useragent.php
+++ lib/class.useragent.php
@@ -1,45 +1,91 @@
 <?php
 	class useragent {
-		private static $detect;
+		private static $mobiledetect;
+		private static $browser;
 		
 		private static function init() {
-			self::$detect = new Mobile_Detect;
+			self::$mobiledetect = new Mobile_Detect;
+			self::$browser = new Browser;
 		}
 		
-		public static function isMobile() {
-			if (empty(self::$detect)) {
-				self::init();
+		//Start - methods from Mobile_Detect
+			public static function isMobile() {
+				if (empty(self::$mobiledetect)) {
+					self::init();
+				}
+				
+				return self::$mobiledetect->isMobile();
 			}
 			
-			return self::$detect->isMobile();
-		}
-		
-		public static function isSmartphone() {
-			return self::isMobile() && !self::isTablet();
-		}
-		
-		public static function isTablet() {
-			if (empty(self::$detect)) {
-				self::init();
+			public static function isSmartphone() {
+				return self::isMobile() && !self::isTablet();
 			}
 			
-			return self::$detect->isTablet();
-		}
+			public static function isTablet() {
+				if (empty(self::$mobiledetect)) {
+					self::init();
+				}
+				
+				return self::$mobiledetect->isTablet();
+			}
+		//End - methods from Mobile_Detect
 		
-		public static function isiOS() {
-			if (empty(self::$detect)) {
-				self::init();
+		//Start - methods from Browser
+			public static function getBrowserVersion() {
+				if (empty(self::$browser)) {
+					self::init();
+				}
+				
+				return self::$browser->getVersion();
 			}
 			
-			return self::$detect->isiOS();
-		}
-		
-		public static function isAndroidOS() {
-			if (empty(self::$detect)) {
-				self::init();
+			public static function isBrowserFirefox() {
+				if (empty(self::$browser)) {
+					self::init();
+				}
+				
+				return self::$browser->getBrowser() == Browser::BROWSER_FIREFOX;
 			}
 			
-			return self::$detect->isAndroidOS();
-		}
+			public static function isBrowserOpera() {
+				if (empty(self::$browser)) {
+					self::init();
+				}
+				
+				return self::$browser->getBrowser() == Browser::BROWSER_OPERA;
+			}
+			
+			public static function isBrowserSafari() {
+				if (empty(self::$browser)) {
+					self::init();
+				}
+				
+				return self::$browser->getBrowser() == Browser::BROWSER_SAFARI;
+			}
+			
+			public static function isBrowserChrome() {
+				if (empty(self::$browser)) {
+					self::init();
+				}
+				
+				return self::$browser->getBrowser() == Browser::BROWSER_CHROME;
+			}
+			
+			public static function isBrowserInternetExplorer() {
+				if (empty(self::$browser)) {
+					self::init();
+				}
+				
+				return self::$browser->getBrowser() == Browser::BROWSER_IE;
+			}
+			
+			public static function isBrowserEdge() {
+				if (empty(self::$browser)) {
+					self::init();
+				}
+				
+				return self::$browser->getBrowser() == Browser::BROWSER_EDGE;
+			}
+		//End - methods from Browser
 	}
 ?>
